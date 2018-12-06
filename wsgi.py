1c84fa9e84953a197c397a656e0782fe7dade824
Mouret, Jérôme <jerome.mouret@sanofi.com>
2018-12-06 13:57:15 +0100

    Test api 9

--- wsgi.py
+++ wsgi.py
@@ -12,7 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
-
+import requests
 from flask import Flask
 application = Flask(__name__)
 
